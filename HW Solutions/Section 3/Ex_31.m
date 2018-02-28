2b7c363595812ec4dd1a2a119bad122116e97252
mtkelley <35580121+mtkelley@users.noreply.github.com>
2018-02-28 10:00:20 -0600

    Add files via upload

--- HW Solutions/Section 3/Ex_31.m
+++ HW Solutions/Section 3/Ex_31.m
@@ -16,31 +16,33 @@ N=1000; %samples
 %Initialize parameters
 Bn=zeros(1,8);
 tau(1:n)=gamrnd(an,bn);
-Bo=Bcond(tau(1),Bn(1,:));
+Bo=Bcond_func(tau(1),Bn(1,:));
 beta(1,:)=Bo;
-Z1(1,:)=Zcond(beta(1,:),tau(1),X,Y);
+Z1(1,:)=Zcond_func(beta(1,:),tau(1),X,Y);
 RHO=zeros(1,N); NAR=zeros(1,N); NGIBBS=zeros(1,N);
 %% Truncated Multivariate Normal
 for i=2:N
     tau(i)=gamrnd(an,bn);
-    beta(i,:)=Bcond(tau(i),Bn(i-1,:));
-    [Z1(i,:), RHO(i),NAR(i),NGIBBS(i)]=Zcond(beta(i,:),tau(i),X,Y);
-    Bn(i,:)=(X'*Z1(i,:)'+Ko*Bo')\Kn;
+    beta(i,:)=Bcond_func(tau(i),Bn(i-1,:));
+    [Z1(i,:), RHO(i),NAR(i),NGIBBS(i)]=Zcond_func(beta(i,:),tau(i),X,Y);
+    Bn(i,:)=Kn\(X'*Z1(i,:)'+Ko*Bo');
 end
 %% Calculate beta mean
 betamean=zeros(1,8);
+start=185; %Throw out the first 185 values
 for i=1:d
-    betamean(i)=sum(beta(:,i),1)/n;
+    beta2=beta(start:n,:);
+    betamean(i)=sum(beta2(:,i),1)/n;
 end
 %% Prediction
 ypred=size(Y);
-for i=1:size(X,1)
-if Z1(N,i)<=0
+for i=1:n
+if X(i,:)*betamean'<=0
     ypred(i)=0;
 else
     ypred(i)=1;
 end
 end
 %% Success of the model
-Dif=sum(Y'-ypred);
-fracright=abs(Dif)/n;
+incorrect=sum(Y'-ypred); %if same, then zero, if different, then -1
+fracright=1-abs(incorrect)/n;
---
+++ HW Solutions/Section 3/Zcond_func.m
@@ -0,0 +1,17 @@
+function [Z,RHO,NAR,NGIBBS] = Zcond_func(beta,tau,X,Y)
+%Conditional z generator based on truncated multivariate normal
+%
+mean=(X*beta');
+stddev=sqrt(1/tau)*eye(size(X,1),size(X,1));
+A=zeros(1,768);
+for i=1:length(Y)
+if Y(i)==1
+   A(1,i)=1;
+elseif Y(i)==0
+   A(1,i)=-1;
+end
+b=0;
+end
+[Z,RHO,NAR,NGIBBS]=rmvnrnd_func(mean,stddev,1,A,b);
+end
+
