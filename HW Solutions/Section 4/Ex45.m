a1047a9530919037f80de78099bef6783ab3dab7
mtkelley <35580121+mtkelley@users.noreply.github.com>
2018-04-13 17:50:16 -0500

    Add files via upload

--- HW Solutions/Section 4/Ex45.m
+++ HW Solutions/Section 4/Ex45.m
@@ -8,19 +8,19 @@ eruptions=table2array(faithful(:,1));
 waiting=table2array(faithful(:,2));
 N=length(waiting);
 
-Y=Scale(eruptions,0,1); X=waiting;
+Y=Scale(eruptions,0,1); X=waiting';
 %% Prediction
 Xtest=linspace(0,100,272)';
 
 alpha=.1;
 sigma=.1;
-l=1;
-kfcn = @(x1,x2) alpha^2*exp(-0.5/(l^2) * sqrt(sum(x1-x2).^2))+sigma^2*eye(size(x1,1));
+l=10;
+kfcn = @(x1,x2) alpha^2*exp(-0.5/(l^2) * sqrt(sum(x1'-x2').^2))+sigma^2*eye(size(x1,1));
 
 Xt=linspace(0,100,272);
-ks=kfcn(Xt,Xt);
-Ks=kfcn(Xt,X');
-ksig=kfcn(X',X');
+ks=kfcn(X,X);
+Ks=kfcn(X,Xt);
+ksig=kfcn(Xt,Xt);
 
 m=Ks.*(inv(ksig+sigma^2*eye(length(ksig)))).*Y';
 Cov=ks-Ks.*inv(ksig+sigma^2*eye(length(ksig)))*Ks';
