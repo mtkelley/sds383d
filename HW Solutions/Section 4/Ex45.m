7d87ddbc410e974ad5886c1f954459ab7da1ca27
mtkelley <35580121+mtkelley@users.noreply.github.com>
2018-04-13 17:41:38 -0500

    Add files via upload

--- HW Solutions/Section 4/Ex45.m
+++ HW Solutions/Section 4/Ex45.m
@@ -18,17 +18,17 @@ l=1;
 kfcn = @(x1,x2) alpha^2*exp(-0.5/(l^2) * sqrt(sum(x1-x2).^2))+sigma^2*eye(size(x1,1));
 
 Xt=linspace(0,100,272);
-ks=kfcn(X',X');
-Ks=kfcn(X',Xt);
-ksig=kfcn(Xt,Xt);
+ks=kfcn(Xt,Xt);
+Ks=kfcn(Xt,X');
+ksig=kfcn(X',X');
 
-m=Ks.*(inv(ksig)).*Y';
-Cov=ks-Ks.*inv(ksig)*Ks';
+m=Ks.*(inv(ksig+sigma^2*eye(length(ksig)))).*Y';
+Cov=ks-Ks.*inv(ksig+sigma^2*eye(length(ksig)))*Ks';
 err=1.96*sqrt(diag(Cov));
 %%
 clf;
 figure(1)
-plot(X,Y,'b*','linewidth',1.5); hold on; plot(X,m,'*')
+plot(X,Y,'b*','linewidth',1.5); hold on; plot(Xt,m)
 ylabel('Eruptions'); xlabel('Waiting')
 legend('Actual','Prediction','Location','Northwest')
 
