40789c4dd22f988e89efb50d1956b1dac02871c3
mtkelley <35580121+mtkelley@users.noreply.github.com>
2018-04-16 18:07:37 -0500

    Add files via upload

--- HW Solutions/Section 5/Ex52Func.m
+++ HW Solutions/Section 5/Ex52Func.m
@@ -4,7 +4,7 @@ n=length(y);
 %Initialize parameter size
 mu=zeros(Ns+1,2);
 omega=zeros(Ns+1,1);
-sigma2=omega;
+sigma2=mu;
 z=zeros(Ns+1,n);
 
 %Initial values
@@ -13,10 +13,10 @@ n0=length(y);
 sumX=0;
 %Sample muo
 s2o=(1/sigmao^2+n0/sigmao^2)^(-1);
-mo=(muo/sigmao^2+(sumX)^2/sigmao^2)*2/(1/sigmao^2+n0/sigmao^2);
+mo=(muo/sigmao^2+(sumX)/sigmao^2)*2/(1/sigmao^2+n0/sigmao^2);
 mu(1,:)=normrnd(mo,s2o);
 omega(1)=1;gamrnd(a,b)*prod(normrnd(0,1/omegao));
-sigma2(1)=1;1/omega(1);
+sigma2(1,:)=1;1/omega(1);
 
 Xsum0=zeros(Ns);
 Xsum1=zeros(Ns);
@@ -27,9 +27,9 @@ for iter=2:Ns
     Xsum1(iter)=0;
     for i=1:n
         %sample zi
-        Pxi1(iter,i)=normrnd(mu(iter-1,2),sigma2(iter-1));
-        Pxi0(iter,i)=normrnd(mu(iter-1,1),sigma2(iter-1));
-        z(iter,i)=binornd(1,abs(Pxi1(iter,i)/((Pxi1(iter,i)+Pxi0(iter,i)))));
+        Pxi1(iter,i)=abs(normrnd(mu(iter-1,2),sigma2(iter-1,2)));
+        Pxi0(iter,i)=abs(normrnd(mu(iter-1,1),sigma2(iter-1,1)));
+        z(iter,i)=binornd(1,Pxi1(iter,i)/((Pxi1(iter,i)+Pxi0(iter,i))));
         %Count number of z occurences in each category
         if (z(iter,i)==0)
             n0=n0+1;
@@ -44,10 +44,10 @@ for iter=2:Ns
     Xcount=[Xsum0 Xsum1];
         
     for k=1:2
-        s2(k)=(1/sigma2(iter-1)+Ncount(k)/sigma2(iter-1))^(-1);
-        m(k)=(muo/sigma2(iter-1)+(Xcount(k))^2/sigma2(iter-1))*2/(1/sigma2(iter-1)+Ncount(k)/sigma2(iter-1));
-        mu(iter,k)=normrnd(m(k),s2(k));
-        sigma2(iter)=gamrnd(a,b)*prod(normrnd(mu(iter,k),sigma2(iter-1)));
+        s2(iter,k)=(1/sigma2(iter-1,k)+Ncount(k)/sigma2(iter-1,k))^(-1);
+        m(iter,k)=(mu(iter-1,k)/sigma2(iter-1,k)+(Xcount(k))/sigma2(iter-1,k))*1/(1/sigma2(iter-1,k)+Ncount(k)/sigma2(iter-1,k));
+        mu(iter,k)=normrnd(m(iter,k),s2(iter,k));
+        sigma2(iter,k)=gamrnd(a,b)*prod(normrnd(mu(iter,k),sigma2(iter-1,k)));
     end
 end
 end
